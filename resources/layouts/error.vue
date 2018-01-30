b0dda93b0742198571d07e59758cf6f177179d58
crypt0666 <crypt0666@inbox.lv>
2018-01-30 08:29:26 +0100

    Upgrade to current nuxt-community/adonuxt-template

--- resources/layouts/error.vue
+++ resources/layouts/error.vue
@@ -1,32 +1,38 @@
+{{{{raw}}}}
 <template>
   <section class="container">
-    <h1>{{ error.statusCode }}</h1>
-    <h2>{{ error.message }}</h2>
-    <nuxt-link :to="{ name: 'index' }" v-if="error.statusCode === 404">Home</nuxt-link>
+    <h1 class="title">
+      {{ error.statusCode }}
+    </h1>
+    <h2 class="info">
+      {{ error.message }}
+    </h2>
+    <nuxt-link class="button" to="/" v-if="error.statusCode === 404">
+      Homepage
+    </nuxt-link>
   </section>
 </template>
-
+{{{{/raw}}}}
 <script>
-  export default {
-    name: 'layout-error',
-
-    props: ['error']
-  }
+export default {
+  props: ['error']
+}
 </script>
 
 <style scoped>
-  h1 {
-    margin-top: 15px;
-    font-size: 5em;
-  }
-
-  h2 {
-    font-weight: 300;
-    color: #9aabb1;
-    margin: 0;
-  }
-
-  a {
-    margin-top: 50px;
-  }
+.title
+{
+  margin-top: 15px;
+  font-size: 5em;
+}
+.info
+{
+  font-weight: 300;
+  color: #9aabb1;
+  margin: 0;
+}
+.button
+{
+  margin-top: 50px;
+}
 </style>
